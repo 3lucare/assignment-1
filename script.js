7e04a25b6ceabcb52fb5e8ae1ce35a6ae27e616d
iga waclawska <iga.waclawska@gmail.com>
2024-10-30 10:20:15 +0100

    add feedback to the assignment-1

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+// Great work on saving elements as variables at the beginning of the script!
+// Also, well done on appropriately using const and let!
+
 let count = 0;
 let countMaxLimit = 11;
 
+// Nice job with the selectors!
+// You could also use querySelector for all the elements below instead of getElementById, but it's entirely up to you!
 const counterDisplay = document.getElementById("counter");
 const increaseButton = document.getElementById("increase");
 const decreaseButton = document.getElementById("decrease");
@@ -13,19 +18,20 @@ function updateCounterDisplay() {
 // Event Listeners
 increaseButton.addEventListener("click", () => {
   count++;
-  if(count >= countMaxLimit){
-    alert("You can't go any higher!")
-    return 
- }
+  if (count >= countMaxLimit) {
+    alert("You can't go any higher!");
+    return;
+  }
   updateCounterDisplay();
 });
 
+// You should move updateCounterDisplay() right after decrementing the count
+// so the displayed number updates after it's reduced, not before.
 decreaseButton.addEventListener("click", () => {
   updateCounterDisplay();
-  if(count > 0)
-    {
-       count--;
-    }
+  if (count > 0) {
+    count--;
+  }
 });
 
 resetButton.addEventListener("click", () => {
